3ed06d222c5d7a647f7bbf8056bda0b1b56299fc
Roger Allen <rallen@gmail.com>
2013-08-23 08:13:36 -0700

    initial working version.  Got sketch from https://github.com/rogerallen/explore_overtone/blob/master/src/explore_overtone/atlas_of_consonance.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject atlas_of_consonance "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "tools to explore some atlas of consonance ideas"
+  :url "http://www.github.com/rogerallen/atlas_of_consonance"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [overtone "0.8.1"]
+                 [quil "1.6.0"]]
   :main atlas-of-consonance.core)
--- src/atlas_of_consonance/core.clj
+++ src/atlas_of_consonance/core.clj
@@ -2,8 +2,24 @@
   (:gen-class))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "accept a number on the commandline and use that to load and run a sketch."
   [& args]
   ;; work around dangerous default behaviour in Clojure
   (alter-var-root #'*read-eval* (constantly false))
-  (println "Hello, World!"))
+  ;; run the appropriate sketch
+  (let [nstr     (if (pos? (count args))
+                   (first args)
+                   "1") ;; default
+        n        (try (read-string nstr)
+                      (catch NumberFormatException e 0))
+        n-ns-str (format "atlas%d" n)
+        f-ns     (str "atlas-of-consonance." n-ns-str)
+        _        (try
+                   (require (symbol f-ns))
+                   (catch java.io.FileNotFoundException e nil))
+        func     (resolve (symbol f-ns "run"))]
+    (if func
+      (do
+        (println f-ns)
+        (func))
+      (println (format "unable to find: %s or %s/run" f-ns f-ns)))))
